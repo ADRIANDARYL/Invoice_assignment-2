a3580003604f4e2a1fe0e9aa48c24cfe7e031bd6
ADRIANDARYL <adriandaryl579@gmail.com>
2025-11-27 10:33:36 +0300

    Invoice assignment

---
+++ Main.java
@@ -0,0 +1,31 @@
+class Invoice {
+    public static void main(String[] args) {
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("         Bill To:                                                                           Ship To:");
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("                                                                                                                                                                                                                        ");
+        System.out.println("         Reg C026-01-0780/2024                                                              DARYL MURIMI CHACHA");
+        System.out.println("         No 23/2,SB1 Colony.                                                                No 23/2,SB1 Colony.");
+        System.out.println("         Ragavendra Nagar, Chennai-600124                                                   Ragavendra Nagar, Chennai-600124 ");
+        System.out.println("         GSTIN:33APFSDF1ZV                                                                  GSTIN:33APFSDF1ZV");
+        System.out.println("         Contact: +91-7869825463                                                            Contact: +91-7869825463");
+        System.out.println("         Email : purchase-team@thendral.com                                                 Email :purchase-team@thendral.com ");
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("                             Payment Date: 7 days from date of delivery                     Payment Terms : 100% against invoice");
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(" S.No   |  Product Code     |     Product Name       |     HSN Code   |    Quantity   |    Units    |     Rate     |      Tax   |   Amount    ");
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("  1     |          105      |     Surf Excel 5kg     |     34011910   |         20    |      nos    |    600.00    |      5%    |   12600.00  ");
+        System.out.println("  2     |          109      |     Rin 1kg            |     34011911   |         25    |      nos    |    85.00     |      5%    |   2231.00   ");
+        System.out.println("  3     |          115      |     Hamam soap 150g    |     34011120   |         50    |      nos    |    60.00     |      5%    |   1575.00   ");
+        System.out.println("  4     |          120      |     Lux Soap 150g      |     34011130   |         35    |      nos    |    53.00     |      5%    |   443.00    ");
+        System.out.println("  5     |          125      |     Dove Soap 125g     |     34011140   |         40    |      nos    |    75.00     |      5%    |   1968.00   ");
+        System.out.println("  6     |          129      |     Vim bar 200g       |     34012010   |         30    |      nos    |    15.00     |      5%    |   315.00    ");
+        System.out.println("  7     |          131      |     Pepsodent 150 g    |     34012020   |         20    |      nos    |    85.00     |      18%   |   3009.00   ");
+        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("                                                                                                                   Total        |   22141.00  ");
+        System.out.println("                                                                                                                   Discounts    |   141.00    ");
+        System.out.println("                                                                                                                   Grand Total  |   22000.00  ");
+        System.out.println("                                                                                                         -------------------------------------");
+    }
+}
